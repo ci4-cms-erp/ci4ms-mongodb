60f105783d7f827e1ec77a8efedfcb9d7873b78e
bertugfahriozer <bertugfahriozer@gmail.com>
2021-06-08 16:53:10 +0300

    bugs fixed.

--- app/Config/MongoConfig.php
+++ app/Config/MongoConfig.php
@@ -6,7 +6,7 @@ class MongoConfig extends BaseConfig
     public $db = 'kun-cms'; //your database
     public $hostname = '127.0.0.1'; //if you use remote server you should change host address
     public $userName = 'beaver';
-    public $password = 'RFVtgb1192';
+    public $password = 'kun12345678';
     public $prefix = 'kun_';
     public $port = '27017'; //if you use different port you should change port address
 }
